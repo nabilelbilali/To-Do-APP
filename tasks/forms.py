990eb7cbe8fa30c6e6e600a801db65d1df747d32
unknown <nabil.elbilali97@gmial.com>
2020-05-02 07:42:49 +0200

    initial commit

---
+++ tasks/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model=Task
+        fields="__all__"
+
+class TaskDeleteForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = "__all__"
+        
+     
